7d8c395b3bdad5b96ec56fbb25f204a67033643b
pepper24Bot <jen@robolabs.biz>
2024-04-08 02:23:17 +0800

    point to https graphql

--- app/services/api.ts
+++ app/services/api.ts
@@ -4,4 +4,5 @@ export const BASE_URL = 'http://localhost:8000'
 export const TWITTER_AUTH = 'https://twitter.com/i/oauth2/authorize'
 
 // TODO: Move this to .env
-export const GRAPHQL_URL = 'http://ec2-44-196-59-240.compute-1.amazonaws.com:8000/subgraphs/name/graphprotocol/ens'
+// export const GRAPHQL_URL = 'http://ec2-44-196-59-240.compute-1.amazonaws.com:8000/subgraphs/name/graphprotocol/ens'
+export const GRAPHQL_URL = 'https://subgraph.rootnameservice.com/subgraphs/name/graphprotocol/ens'
