fcc8810642e248a1a88095d7368a800915670d97
pepper24Bot <jen@robolabs.biz>
2024-04-28 13:42:02 +0800

    trial

--- app/services/constants.ts
+++ app/services/constants.ts
@@ -5,7 +5,7 @@ export const DEFAULT_DEBOUNCE = 500;
 /** https://explorer.rootnet.live/tokens */
 export const PAYMENT_METHOD = [
     { label: "ROOT", decimals: 6, address: "0xcCcCCccC00000001000000000000000000000000" },
-    { label: "USDC", decimals: 18, address: "0xCCcCCcCC00000C64000000000000000000000000" }
+    { label: "USDC", decimals: 6, address: "0xCCcCCcCC00000C64000000000000000000000000" }
 ]
 
 export const SORTING_OPTIONS = [
