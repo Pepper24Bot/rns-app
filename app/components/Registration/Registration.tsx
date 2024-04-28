e5e4928666d8ff92ee3f7a0b5f05a415ca51e5cf
pepper24Bot <jen@robolabs.biz>
2024-04-28 13:25:27 +0800

    add console.log getbalance of

--- app/components/Registration/Registration.tsx
+++ app/components/Registration/Registration.tsx
@@ -220,6 +220,7 @@ export const RegisterName: React.FC = () => {
   // On initial load - check wallet balance before doing transaction
   useEffect(() => {
     const getBalanceOf = async () => {
+      console.log("getbalance-rentFee:: ", rentFee);
       if (address) {
         const { data } = await getBalance({
           address,
