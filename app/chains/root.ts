2e620d7fda4dcda5bf7761bcaadbb2b80834033a
pepper24Bot <jen@robolabs.biz>
2024-05-18 20:55:53 +0800

    update the contracts

--- app/chains/root.ts
+++ app/chains/root.ts
@@ -7,61 +7,61 @@
 import { Address, type Chain } from 'viem'
 
 const addresses = {
-    7668: {
-        ensBaseRegistrarImplementation: {
-            address: '0x5E89B2f53DAa0f09B4B8f194dC1a1a2C343bFCFe' as Address,
-        },
-        ensBulkRenewal: {
-            address: '0x29435cd3D16647cc4e9eB064583E651A11C932b1' as Address,
-        },
-        ensEthRegistrarController: {
-            address: '0x25ED7268B38c5C0095a1C40dbA795BA10D6b46C9' as Address,
-        },
-        ensNameWrapper: {
-            address: '0x523abeFB947d78F96cBeC1ee8b101810B9b7453F' as Address,
-        },
-        ensPublicResolver: {
-            address: '0xc27B94BbA415129E52075D63C5dA070B8f9582C0' as Address,
-        },
-        ensRegistry: {
-            address: '0x41f6B3bA30B927d980Edb2B965C2c00dF181Be21' as Address,
-        },
-        ensReverseRegistrar: {
-            address: '0x6b6e0e9f40A82B65CE4f144EAbcce1b53e408Be7' as Address,
-        },
-        ensUniversalResolver: {
-            address: '0x02ea09723Cf472585EA2B1759a5b53710ADf7b8B' as Address,
-        },
+  7668: {
+    ensBaseRegistrarImplementation: {
+      address: "0xEeD3C3c547751e23020f4cb506FbA37baEb3308D" as Address,
     },
-}
+    ensBulkRenewal: {
+      address: "0x0193eFBF9504422700295C022766891b0b10049F" as Address,
+    },
+    ensEthRegistrarController: {
+      address: "0xc85E5802BADE56Facb93bb373da6bA7c1902b19c" as Address,
+    },
+    ensNameWrapper: {
+      address: "0x44640D662A423d738D5ebF8B51E57AfC0f2cf4Df" as Address,
+    },
+    ensPublicResolver: {
+      address: "0x870bC2604D6EAC536c791A603bFDE1A1448e168e" as Address,
+    },
+    ensRegistry: {
+      address: "0xEC58C26B8E0A4bc0fe1ad21D216e4ecAd9e037A8" as Address,
+    },
+    ensReverseRegistrar: {
+      address: "0xfFF7719aaB38eadE6A1CfdA864a174B715e9d673" as Address,
+    },
+    ensUniversalResolver: {
+      address: "0x7808dF0A1F1d58c6Ad0F3bA07E749D730F02f13A" as Address,
+    },
+  },
+};
 
 export const root = {
-    id: 7668,
-    name: 'The Root Network',
-    nativeCurrency: { name: 'Ripple', symbol: 'XRP', decimals: 18 },
-    rpcUrls: {
-        default: {
-            http: ['https://root.rootnet.live/'],
-        },
+  id: 7668,
+  name: 'The Root Network',
+  nativeCurrency: { name: 'Ripple', symbol: 'XRP', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://root.rootnet.live/'],
     },
-    blockExplorers: {
-        default: {
-            name: 'Root Mainnet',
-            url: 'https://rootscan.io/',
-        },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Root Mainnet',
+      url: 'https://rootscan.io/',
     },
-    contracts: {
-        ...addresses[7668],
-    },
+  },
+  contracts: {
+    ...addresses[7668],
+  },
 } as const satisfies Chain
 
 /**
  * Use this config to programatically add this network to a wallet
  */
 export const rootWalletConfig = {
-    chainId: "0x1DF4",
-    chainName: 'The Root Network',
-    nativeCurrency: { name: 'Ripple', symbol: 'XRP', decimals: 18 },
-    rpcUrls: ['https://root.rootnet.live/'],
-    blockExplorerUrls: ['https://rootscan.io/'],
+  chainId: "0x1DF4",
+  chainName: 'The Root Network',
+  nativeCurrency: { name: 'Ripple', symbol: 'XRP', decimals: 18 },
+  rpcUrls: ['https://root.rootnet.live/'],
+  blockExplorerUrls: ['https://rootscan.io/'],
 }
