c1ca708afa7a197797b3fcb28e158e25e0964676
pepper24Bot <jen@robolabs.biz>
2024-05-20 07:48:13 +0800

    add server-side stylesheet

---
+++ app/(pages)/_document.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <div id="myportal" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+/**
+ * Server Side Rendering Issue in MUI
+ * Inject the style before rendering the page.
+ *
+ * See: https://mui.com/material-ui/guides/server-rendering/
+ */
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
