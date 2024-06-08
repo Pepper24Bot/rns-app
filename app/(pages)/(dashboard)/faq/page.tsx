39505c9fc73ff34133c58ff1443f62795ab36d28
pepper24Bot <jen@robolabs.biz>
2024-06-08 19:21:15 +0800

    routing: implement /faq

---
+++ app/(pages)/(dashboard)/faq/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Dashboard from "@/components/Dashboard/Dashboard";
+import { scrollIntoElement } from "@/utils/common";
+import FrequentlyAsked from "@/components/Dashboard/Tab/Faq/Faq";
+
+export default function Page() {
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (hasMounted) {
+      // scroll into element
+      scrollIntoElement("Dashboard-Container");
+    }
+  }, [hasMounted]);
+
+  return (
+    <Dashboard hasMounted={hasMounted}>
+      <FrequentlyAsked />
+    </Dashboard>
+  );
+}
