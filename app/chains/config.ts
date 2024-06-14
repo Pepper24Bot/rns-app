b87a205d5a9494c4dc64f177e2763888901b69b0
pepper24Bot <jen@robolabs.biz>
2024-06-14 17:33:29 +0800

    add phantom wallet

--- app/chains/config.ts
+++ app/chains/config.ts
@@ -20,6 +20,7 @@ export const config = createConfig({
                 explorerExcludedWalletIds: "ALL",
                 explorerRecommendedWalletIds: [
                     "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+                    "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393",
                     "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa",
                     "19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927",
                     "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369"
