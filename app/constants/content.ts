1f392f8531de9f71c226524b2a8637b230b06f8f
pepper24Bot <jen@robolabs.biz>
2024-06-25 22:59:42 +0800

    include all share message variations

--- app/constants/content.ts
+++ app/constants/content.ts
@@ -1,13 +1,13 @@
 export const TWEETS_RNS = [
-    // `Don’t be known as 0xFffF..00D4FR. Be known as legend.𝗿𝗼𝗼𝘁✨\n\nWith @RootNameService, my wallet, gaming and social identities are unified on @TheRootNetwork. Register your Identity and share it to enter monthly raffles for prizes!🎁\n\n🔗rootnameservice.com`,
+    `Don’t be known as 0xFffF..00D4FR. Be known as legend.𝗿𝗼𝗼𝘁✨\n\nWith @RootNameService, my wallet, gaming and social identities are unified on @TheRootNetwork. Register yours and share it to enter monthly raffles for prizes!🎁\n\n🔗rootnameservice.com`,
 
-    `Why be 0xFffF..00D4FR? Be known as legend.𝗿𝗼𝗼𝘁🌐✨\n\n@RootNameService lets me unify my wallet, gaming, and social identities on @TheRootNetwork. Register yours and share to join monthly raffles for prizes!🎁\n\n🔗rootnameservice.com`,
+    `Why be 0xFffF..00D4FR? Be known as legend.𝗿𝗼𝗼𝘁 🌐✨\n\n@RootNameService lets me unify my wallet, gaming, and social identities on @TheRootNetwork. Register yours and share to join monthly raffles for prizes!🎁\n\n🔗rootnameservice.com`,
 
-    // `Thrilled to have my @RootNameService Identity on @TheRootNetwork!💫 @Ethereum and #Web3 users, secure the .𝗿𝗼𝗼𝘁 equivalent of your digital Identity and share it to join monthly raffles for prizes!🎁\n\nHow will you 𝗕𝗲 𝗞𝗻𝗼𝘄𝗻?✨\n\n🔗rootnameservice.com`,
+    `Thrilled to have my @RootNameService Identity on @TheRootNetwork!💫 #Web3 users, secure the .𝗿𝗼𝗼𝘁 equivalent of your digital Identity and share it to join monthly raffles for prizes!🎁\n\nHow will you 𝗕𝗲 𝗞𝗻𝗼𝘄𝗻?✨\n\n🔗rootnameservice.com`,
 
-    // `Why be a random string like 0xFff..0D4FR? Be known as legend.𝗿𝗼𝗼𝘁🌐✨\n\nWith @RootNameService, I’ve streamlined my wallet, gaming, and social identities on @TheRootNetwork. Register your Identity and share it to enter into monthly raffles!🎁\n\n🔗rootnameservice.com`,
+    `Why be a random string like 0xFff..0D4FR? Be known as legend.𝗿𝗼𝗼𝘁✨\n\nWith @RootNameService, streamline your wallet, gaming, & social identities on @TheRootNetwork. Register yours and share it to enter into monthly raffles!🎁\n\n🔗rootnameservice.com`,
 
-    // `Don’t be known as 0xFff..0D4FR. Be known as legend.𝗿𝗼𝗼𝘁 🌐✨\n\nWith @RootNameService, I've got my wallet, gaming, and social identity all in one on @TheRootNetwork. Register your own and share it to enter into monthly raffles for prizes!🎁\n\n🔗rootnameservice.com`,
+    `Don’t be known as 0xFf..D4FR. Be known as legend.𝗿𝗼𝗼𝘁✨\n\nWith @RootNameService, get your wallet, gaming & social identity all in one on @TheRootNetwork. Register yours and share it to enter into monthly raffles for prizes!🎁\n\n🔗rootnameservice.com`,
 
     `Thrilled to have my RNS Identity on @TheRootNetwork! My .𝗿𝗼𝗼𝘁 extension ensures seamless experiences and easy identity management.\n\nRegister, link to FuturePass, and share yours to join monthly raffles for great prizes!🎁✨\n\n🔗rootnameservice.com`,
 ]
