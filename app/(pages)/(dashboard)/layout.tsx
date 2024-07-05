8a777348d47c4e2e5c360348a0681e2d36110ec9
pepper24Bot <jen@robolabs.biz>
2024-07-05 14:44:32 +0800

    fix build errors

--- app/(pages)/(dashboard)/layout.tsx
+++ app/(pages)/(dashboard)/layout.tsx
@@ -1,4 +1,3 @@
-import Dashboard from "@/components/Dashboard/Dashboard";
 import MainPage from "@/components/Main/MainPage";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
